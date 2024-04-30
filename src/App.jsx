21f3476ec785f929be7f7ed4d1f04a589c6f35fe
Alejandro Bermúdez Holguín <alejo0395@gmail.com>
2024-04-30 00:12:49 -0500

    Kickoff: Tailwind, react-router, react-hook-form, react-query, eslint+prettier

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+export const App = () => (
+  <h1 className="text-3xl font-bold underline">Learning Camp 2024 TP!</h1>
+);
---
+++ src/Routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { App } from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
+export const Routes = () => <RouterProvider router={router} />;
